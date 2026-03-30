f1d7c6737d09a023ded65b96db092362ff5a8463
Stijn Veenman <veenman.stijn@gmail.com>
2026-03-30 21:37:00 +0200

    setup nh module

--- modules/hosts/omarchy/default.nix
+++ modules/hosts/omarchy/default.nix
@@ -3,14 +3,22 @@
   inputs,
   ...
 }: {
-  flake.homeConfigurations.omarchy = inputs.home-manager.lib.homeManagerConfiguration {
+  flake.homeConfigurations.stiixxy = inputs.home-manager.lib.homeManagerConfiguration {
     pkgs = import inputs.nixpkgs {system = "x86_64-linux";};
     modules = [
+      self.homeModules.nh
       {
         home.username = "stiixxy";
         home.homeDirectory = "/home/stiixxy/";
         home.stateVersion = "25.11";
       }
     ];
+  };
+
+  flake.homeModules.nh = {
+    programs.nh = {
+      enable = true;
+      homeFlake = "/home/stiixxy/nixconf";
+    };
   };
 }
