a120e227bb9d7720d2e48683e8c4c3e00c8eef91
Stijn Veenman <veenman.stijn@gmail.com>
2026-03-30 22:02:38 +0200

    enable home-manager bash for shellVariables

--- modules/hosts/omarchy/default.nix
+++ modules/hosts/omarchy/default.nix
@@ -3,17 +3,41 @@
   inputs,
   ...
 }: {
-  flake.homeConfigurations.stiixxy = inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = import inputs.nixpkgs {system = "x86_64-linux";};
-    modules = [
-      self.homeModules.nh
-      {
-        home.username = "stiixxy";
-        home.homeDirectory = "/home/stiixxy/";
-        home.stateVersion = "25.11";
-      }
-    ];
-  };
+  flake.homeConfigurations.stiixxy =
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {system = "x86_64-linux";};
+      modules = [
+        self.homeModules.nh
+        {
+          home.username = "stiixxy";
+          home.homeDirectory = "/home/stiixxy/";
+          home.stateVersion = "25.11";
+
+          programs.bash = {
+            enable = true;
+            bashrcExtra = "
+# If not running interactively, don't do anything (leave this at the top of this file)
+[[ $- != *i* ]] && return
+
+# All the default Omarchy aliases and functions
+# (don't mess with these directly, just overwrite them here!)
+source ~/.local/share/omarchy/default/bash/rc
+
+# Add your own exports, aliases, and functions here.
+#
+# Make an alias for invoking commands you use constantly
+# alias p='python'
+
+eval \"$(direnv hook bash)\"
+
+removebg() {
+  magick \"$1\" -fuzz 10% -transparant \"$2:white\" \"$1\"
+}
+          ";
+          };
+        }
+      ];
+    };
 
   flake.homeModules.nh = {
     programs.nh = {
