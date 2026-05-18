4a8108221ace4970dc00c3069b4448679ab93a8f
Stijn Veenman <stijn.veenman@schiphol.nl>
2026-05-18 15:56:59 +0200

    copy over home main home settings

--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -1,6 +1,29 @@
-{...}: {
+{pkgs, ...}: {
   home.username = "sv";
   home.homeDirectory = "/Users/sv";
 
   home.stateVersion = "24.05";
+
+  home.sessionVariables = {
+    RUSH_PARALLELISM = "60%";
+    PC_HIDE_DISABLED_PROC = "1";
+  };
+
+  programs.git.userEmail = "stijn.veenman@schiphol.nl";
+
+  home.packages = [
+    pkgs._1password-cli
+  ];
+
+  programs.zsh = {
+    shellAliases = {
+      ri = "rush install";
+      rb = "rush build";
+      rbt = "rush build --to";
+
+      ac = "cd ~/Documents/airport-control/";
+
+      lg = "lazygit";
+    };
+  };
 }
