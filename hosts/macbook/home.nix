df7ec54a2228fdbcd6b862431607c8e496ac78dd
Stijn Veenman <stijn.veenman@schiphol.nl>
2026-05-18 16:11:16 +0200

    add all features from previous repo

--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -11,8 +11,6 @@
     PC_HIDE_DISABLED_PROC = "1";
   };
 
-  programs.git.userEmail = "stijn.veenman@schiphol.nl";
-
   home.packages = [
     pkgs._1password-cli
 
@@ -28,6 +26,29 @@
   ];
 
   programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+      ];
+      theme = "robbyrussell";
+    };
+
     shellAliases = {
       ri = "rush install";
       rb = "rush build";
@@ -40,4 +61,56 @@
   };
 
   programs.home-manager.enable = true;
+
+  programs.gh = {
+    enable = true;
+    extensions = [pkgs.gh-poi];
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Stijn Veenman";
+    userEmail = "stijn.veenman@schiphol.nl";
+    signing = {
+      signByDefault = true;
+      # autodetect based on commit
+      key = null;
+    };
+  };
+
+  programs.atuin = {
+    enable = true;
+    flags = ["--disable-up-arrow"];
+    settings = {
+      inline_height = 15;
+    };
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      quitOnTopLevelReturn = true;
+      git.overrideGpg = false;
+      git.autoFetch = false;
+
+      gui = {
+        branchColors = {
+          config = "#11aaff";
+        };
+      };
+    };
+  };
 }
