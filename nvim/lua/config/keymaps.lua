1521628a3beaf318dfc922ee9720513ebc47def0
Stijn Veenman <stijn.veenman@schiphol.nl>
2026-06-04 12:49:53 +0200

    config updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,9 @@ local Util = require("lazyvim.util")
 vim.keymap.set("n", "<C-g>", function()
   Snacks.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
 end, { desc = "Lazygit (root dir)" })
+
+-- move highlighted text around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line up" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line down" })
+
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Escape insert mode" })
