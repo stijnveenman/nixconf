f4b91bbacb537ee14c03165277901ab4e489f02f
Stijn Veenman <veenman.stijn@gmail.com>
2026-07-09 07:05:30 +0200

    update git settings

--- hosts/nnn/default.nix
+++ hosts/nnn/default.nix
@@ -5,34 +5,34 @@
 }:
 
 nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-	          home-manager.nixosModules.home-manager
+  system = "x86_64-linux";
+  modules = [
+    ./configuration.nix
+    home-manager.nixosModules.home-manager
 
     {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-	    home-manager.users.stiixxy = {
-    home.stateVersion = "26.05";
-	    imports = [
-    ../../modules/neovim.nix
-	    ];
-	    programs.bash.enable = true;
-  programs.git = {
-  enable = true;
-  
-    settings = {
-      user = {
-        name  = "Stijn Veenman";
-        email = "veenman.stijn@gmail.com";
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.stiixxy = {
+        home.stateVersion = "26.05";
+        imports = [
+          ../../modules/neovim.nix
+        ];
+        programs.bash.enable = true;
+        programs.git = {
+          enable = true;
+
+          settings = {
+            user = {
+              name = "Stijn Veenman";
+              email = "veenman.stijn@gmail.com";
+            };
+            init.defaultBranch = "main";
+            pull.rebase = true;
+          };
+        };
+
       };
-      init.defaultBranch = "main";
-    };
-  };
-
-
-	    };
-          }
-      ];
     }
+  ];
+}
