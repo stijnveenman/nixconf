5878f51d9de1cecd4f7bf8e7dd9edd6621cbc1c0
Stijn Veenman <veenman.stijn@gmail.com>
2026-07-12 17:27:28 +0200

    start on initial keybindings

--- hosts/nnn/home.nix
+++ hosts/nnn/home.nix
@@ -18,12 +18,45 @@
     programs.niri.settings = {
       spawn-at-startup = [
         {argv = ["${lib.getExe pkgs.noctalia-shell}"];}
+        {argv = ["${lib.getExe pkgs._1password-gui} --silent"];}
       ];
 
       binds = {
-        "Mod+Return".action.spawn-sh = lib.getExe pkgs.ghostty;
         "Mod+W".action.close-window = {};
+        "Mod+Return".action.spawn-sh = lib.getExe pkgs.ghostty;
+        "Mod+Shift+Slash".action.spawn-sh = "${lib.getExe pkgs._1password-gui} --toggle";
+
         "Mod+Space".action.spawn-sh = "${lib.getExe pkgs.noctalia-shell} ipc call launcher toggle";
+        "Mod+Grave".action.spawn-sh = "${lib.getExe pkgs.noctalia-shell} ipc call sessionMenu toggle";
+        "Mod+Shift+Grave".action.spawn-sh = "${lib.getExe pkgs.noctalia-shell} ipc call lockScreen lock";
+
+        "Mod+N".action.focus-workspace-down = {};
+        "Mod+P".action.focus-workspace-up = {};
+
+        "Mod+1".action.focus-workspace = 1;
+        "Mod+2".action.focus-workspace = 2;
+        "Mod+3".action.focus-workspace = 3;
+        "Mod+4".action.focus-workspace = 4;
+        "Mod+5".action.focus-workspace = 5;
+        "Mod+6".action.focus-workspace = 6;
+        "Mod+7".action.focus-workspace = 7;
+        "Mod+8".action.focus-workspace = 8;
+        "Mod+9".action.focus-workspace = 9;
+        "Mod+0".action.focus-workspace = 0;
+
+        "Mod+Shift+1".action.move-window-to-workspace = 1;
+        "Mod+Shift+2".action.move-window-to-workspace = 2;
+        "Mod+Shift+3".action.move-window-to-workspace = 3;
+        "Mod+Shift+4".action.move-window-to-workspace = 4;
+        "Mod+Shift+5".action.move-window-to-workspace = 5;
+        "Mod+Shift+6".action.move-window-to-workspace = 6;
+        "Mod+Shift+7".action.move-window-to-workspace = 7;
+        "Mod+Shift+8".action.move-window-to-workspace = 8;
+        "Mod+Shift+9".action.move-window-to-workspace = 9;
+        "Mod+Shift+0".action.move-window-to-workspace = 0;
+
+        "Mod+h".action.focus-column-left = {};
+        "Mod+l".action.focus-column-right = {};
       };
     };
 
