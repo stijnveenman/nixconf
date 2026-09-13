0a448951bf5d89cf310c63357911aac99f130a4d
Stijn Veenman <stijn.veenman@schiphol.nl>
2026-09-13 10:37:31 +0200

    tmux: preserve pane cwd and enable truecolor

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -3,7 +3,7 @@
   config,
   ...
 }: {
-  home.packages = [pkgs.tmux];
+  home.packages = [pkgs.tmux pkgs.reattach-to-user-namespace];
 
   xdg.configFile."tmux/plugins/tpm" = {
     source = pkgs.fetchFromGitHub {
@@ -16,11 +16,27 @@
 
   xdg.configFile."tmux/tmux.conf".text = ''
     # ---Key bindings
+    set -g prefix C-s
+    set -g mouse on
+
     unbind r
     bind r source-file ${config.xdg.configHome}/tmux/tmux.conf
 
-    set -g prefix C-s
-    set -g mouse on
+    bind C-s 'send-keys C-s'
+
+    bind v split-pane -h -c "#{pane_current_path}"
+    bind - split-pane -v -c "#{pane_current_path}"
+
+    bind n next-window
+    bind p previous-window
+    bind c new-window
+
+    bind g display-popup -w 80% -h 80% lazygit
+
+    set-option -g pane-border-status top
+    set-option -g pane-border-lines heavy
+    set-option -g pane-border-indicators off
+    set-option -g pane-border-format ""
 
     # --- Sensible defaults
     set -s escape-time 0
@@ -29,9 +45,12 @@
     set -g status-interval 5
     # (OS X) Fix pbcopy/pbpaste for old tmux versions (pre 2.6)
     set -g default-command "reattach-to-user-namespace -l $SHELL"
-    set -g default-terminal "screen-256color"
+    set -g default-terminal "tmux-256color"
+    set -as terminal-features ",*:RGB"
     set -g status-keys emacs
     set -g focus-events on
+    set -g extended-keys on
+    set -g extended-keys-format csi-u
     setw -g aggressive-resize on
 
     set -g @plugin 'tmux-plugins/tpm'
@@ -43,8 +62,8 @@
     set -g @tmux-gruvbox-right-status-z ' '
 
     set -g @plugin 'christoomey/vim-tmux-navigator'
-    set -g @vim_navigator_mapping_left "C-Left"
-    set -g @vim_navigator_mapping_right "C-Right"
+    set -g @vim_navigator_mapping_left "C-h"
+    set -g @vim_navigator_mapping_right "C-l"
     set -g @vim_navigator_mapping_up "C-k"
     set -g @vim_navigator_mapping_down "C-j"
     set -g @vim_navigator_mapping_prev ""
