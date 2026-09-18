4e05cd12b91ac57678b80e3a29e905636252ce99
Stijn Veenman <stijn.veenman@schiphol.nl>
2026-09-18 10:47:11 +0200

    fix diff treesitter query

---
+++ nvim/queries/diff/highlights.scm
@@ -0,0 +1,51 @@
+; Compatibility query for Neovim nightly's bundled tree-sitter-diff parser.
+; Remove once its parser is updated to grammar v0.2.
+(comment) @comment @spell
+
+[
+  (addition)
+  (new_file)
+] @diff.plus
+
+[
+  (deletion)
+  (old_file)
+] @diff.minus
+
+(commit) @constant
+
+(location) @attribute
+
+(command
+  "diff" @function
+  (argument) @variable.parameter)
+
+(filename) @string.special.path
+
+(mode) @number
+
+([
+  ".."
+  "+"
+  "++"
+  "+++"
+  "++++"
+  "-"
+  "--"
+  "---"
+  "----"
+] @punctuation.special
+  (#set! priority 95))
+
+[
+  (binary_change)
+  (similarity)
+  (file_change)
+] @label
+
+(index
+  "index" @keyword)
+
+(similarity
+  (score) @number
+  "%" @number)
